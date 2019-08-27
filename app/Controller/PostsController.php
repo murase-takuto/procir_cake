0a437503ab11d9a6af5d8c63a08a50e80382b2db
murase-takuto <takumura0911@gmail.com>
2019-08-27 18:09:41 +0900

    [update] 記述の追加
    ブログチュートリアルに沿って記述を追加しました。

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,12 +1,13 @@
 <?php
 class PostsController extends AppController {
-    public $helpers = array('Html', 'Form');
+    public $helpers = array('Html', 'Form', 'Flash');
+    public $components = array('Flash');
 
     public function index() {
         $this->set('posts', $this->Post->find('all'));
     }
 
-	public function view($id = null) {
+	public function view($id) {
 		if (!$id) {
 			throw new NotFoundException(__('Invalid post'));
 		}
@@ -16,6 +17,17 @@ class PostsController extends AppController {
 			throw new NotFoundException(__('Invalid post'));
 		}
 		$this->set('post', $post);
+	}
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Post->create();
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success(__('Your post has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Flash->error(__('Unable to add your post.'));
+		}
 	}
 }
 ?>
