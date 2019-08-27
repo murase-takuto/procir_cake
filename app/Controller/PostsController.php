fc10e63b70b7182d17349755164dc42deca55dd1
murase-takuto <takumura0911@gmail.com>
2019-08-27 18:49:40 +0900

    [update] 記述の追加
    ブログチュートリアルに沿って編集処理をする記述を追加しました。

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -7,7 +7,7 @@ class PostsController extends AppController {
         $this->set('posts', $this->Post->find('all'));
     }
 
-	public function view($id) {
+	public function view($id = null) {
 		if (!$id) {
 			throw new NotFoundException(__('Invalid post'));
 		}
@@ -27,6 +27,30 @@ class PostsController extends AppController {
 				return $this->redirect(array('action' => 'index'));
 			}
 			$this->Flash->error(__('Unable to add your post.'));
+		}
+	}
+
+	public function edit($id = null) {
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+
+		$post = $this->Post->findById($id);
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Post->id = $id;
+			if ($this->Post->save($this->request->data)) {
+				$this->Flash->success(__('Your post has been updated.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Flash->error(__('unable to update your post.'));
+		}
+
+		if (!$this->request->data) {
+			$this->request->data = $post;
 		}
 	}
 }
