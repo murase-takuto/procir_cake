61720b3ba583a4845f9fff726e9f99a173bc9f06
murase-takuto <takumura0911@gmail.com>
2019-08-29 17:09:41 +0900

    [fix] 記述の修正
    インデントを修正しました。

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,11 +1,11 @@
 <?php
 class PostsController extends AppController {
-    public $helpers = array('Html', 'Form', 'Flash');
-    public $components = array('Flash');
+	public $helpers = array('Html', 'Form', 'Flash');
+	public $components = array('Flash');
 
-    public function index() {
-        $this->set('posts', $this->Post->find('all'));
-    }
+	public function index() {
+	$this->set('posts', $this->Post->find('all'));
+	}
 
 	public function view($id = null) {
 		if (!$id) {
