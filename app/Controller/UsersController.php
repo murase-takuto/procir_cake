11ad4c943e06ef68f3d1c1508ec40c90e1f6079a
murase-takuto <takumura0911@gmail.com>
2019-09-01 11:52:53 +0900

    [fix] 記述の修正
    #31 ユーザー登録後のリダイレクト先を /posts/index に変更しました。

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -28,7 +28,7 @@ class UsersController extends AppController {
 			$this->User->create();
 			if ($this->User->save($this->request->data)) {
 				$this->Flash->success(__('The user has been saved'));
-				return $this->redirect(array('action' => 'index'));
+				return $this->redirect(array('action' => '../posts/index'));
 			}
 			$this->Flash->error(
 				__('The user could not be saved. Please, try again.')
