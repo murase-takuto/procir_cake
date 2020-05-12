74560e82d5d5157c7a3c54d7b265ea8f62689f0a
murase-takuto <takumura0911@gmail.com>
2020-05-12 10:24:40 +0900

    [add]ファイル追加

---
+++ app/webroot/img/レシート受取アプリ　マインドマップ.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1568820014127" ID="ID_850103739" MODIFIED="1568820014127" TEXT="&#x65b0;&#x898f;&#x30de;&#x30a4;&#x30f3;&#x30c9;&#x30de;&#x30c3;&#x30d7;"/>
+</map>
